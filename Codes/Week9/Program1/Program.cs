17c0a11d41815a729c189c93cde0d04a3cd0431d
JaeHyeokSeong <seong5763@gmail.com>
2022-09-23 19:38:59 +1000

    upload week9 program1 code

---
+++ Codes/Week9/Program1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Program1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
